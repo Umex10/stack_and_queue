b97ce44ac9f31d8d6f9c36cd6b75dd2e7039fec0
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-05-17 22:45:29 +0200

    Finished with Javadoc Comments on MyQueueList

--- src/main/java/at/fhj/msd/MyQueueList.java
+++ src/main/java/at/fhj/msd/MyQueueList.java
@@ -1,5 +1,12 @@
 package at.fhj.msd;
 
+/**
+ * The MyQueueList class implements a queue using a doubly linked list.
+ * It provides methods to add elements to the queue and remove elements from it.
+ * It works with generic types and works like a regular queue, so Elements are added to
+ * the back of the queue and when Elements are deleted from the queue, 
+ * the first Element added gets deleted.
+ */
 public class MyQueueList<E> {
 
       private E Element;
@@ -9,16 +16,40 @@ public class MyQueueList<E> {
       private Node<E> tail;
       private Node<E> pointer;
       
+      /**
+       * Constructs a new MyQueueList with the specified initial node.
+       *
+       * @param NodeElement the initial node of the queue
+       */
       public MyQueueList (Node<E> NodeElement)
       {
             this.head = NodeElement;
             this.tail = NodeElement;
       }
 
+      /**
+       * the size parameter is used to keep track of the number of elements in the queue.
+       * It is initialized to 1 because the queue starts with one element.
+       * It then increments by 1 every time an element is added to the queue.
+       * It decrements by 1 every time an element is removed from the queue.
+       * @return the number of elements currently in the queue.
+       */
       public int size() {return this.size;}
 
+      /**
+       * Checks if the queue is empty.
+       * @return true if the queue is empty, false otherwise.
+       */
       public boolean isEmpty() {return size == 0;}
 
+      /**
+       * Adds a new element to the back of the queue.
+       * This method creates a new node with the given element and updates the tail pointer.
+       * If the queue is empty, it also sets the head pointer to the new node.
+       * If the queue has only one element, it updates the head's previous pointer to point to the new node.
+       * If the queue has two or more elements, it updates the pointer to point to the new node.
+       * @param Element the element to be added to the queue.
+       */
       public void put(E Element)
       {
             Node<E> newNode = new Node<>(Element);
@@ -29,6 +60,13 @@ public class MyQueueList<E> {
             ++this.size;
       }
 
+      /**
+       * Removes and returns the first element from the queue.
+       * This method updates the head pointer to point to the previous node in the queue.
+       * If the queue is empty, it throws an IllegalArgumentException.
+       * @return the removed element from the queue.
+       * @throws IllegalArgumentException if the queue is empty.
+       */
       public E get()
       {
             if (isEmpty() == true) 
